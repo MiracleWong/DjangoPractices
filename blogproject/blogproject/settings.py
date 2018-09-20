2972a665f599783e79007179d25454bee5e47f30
MiracleWong <cfwr1991@126.com>
2018-09-20 22:41:22 +0800

    modify the static file path

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print("Base DIR" + BASE_DIR)
+# print("Base DIR: " + BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -113,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
