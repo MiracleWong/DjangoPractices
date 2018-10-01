5e815cc341ec38bf2bc1a7e665eac24c8e1a110e
MiracleWong <cfwr1991@126.com>
2018-10-01 16:44:36 +0800

    增加登录功能

---
+++ mysite/account/urls.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^login/$', views.user_login, name='user_login'),
+]
